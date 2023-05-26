8c765e6f4b2c577e3007eb3835e779998b8e4138
rehacsi <rehacsi@student.uzlabina.cz>
2023-05-26 18:09:43 +0200

    commit

--- src/app/lunch/lunch.component.ts
+++ src/app/lunch/lunch.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {LunchModel} from './lunch.model'
 import {FormControl} from "@angular/forms";
+import {UserModel} from "./user.model";
 
 @Component({
   selector: 'app-lunch',
@@ -15,10 +16,17 @@ export class LunchComponent implements OnInit {
   }
 
   today: number = Date.now();
-
-  showResult = false;
+  data: any = {};
+  selectedUserValue: number = -1;
   selectedLunchValue: number = -1;
   selectedLunch: LunchModel | undefined = undefined;
+  selectedUser: UserModel | undefined = undefined;
+
+  usernames: UserModel[] = [
+    {username: "Zbyňa", value: 10},
+    {username: "Zbyňa1", value: 11},
+    {username: "Zbyňa2", value: 12}
+  ]
 
   lunchItems: LunchModel[] = [
     {name: 'Guláš', value: 1},
@@ -27,21 +35,25 @@ export class LunchComponent implements OnInit {
     {name: "vegan", value: 4}
   ];
 
+
   submitForm() {
     console.log('selected', this.selectedLunchValue)
     console.log('lunch items', this.lunchItems)
-    if (this.selectedLunchValue !== null) {
+    if (this.selectedLunchValue !== null && this.selectedUserValue !== null) {
       this.selectedLunch = this.lunchItems.find((o) => o.value === +this.selectedLunchValue);
+      this.selectedUser = this.usernames.find((o) => o.value === +this.selectedUserValue);
 
-      if (this.selectedLunch !== undefined) {
+      if (this.selectedLunch !== undefined && this.selectedUser !== undefined) {
         console.log('Oběd:', this.selectedLunch.name);
         console.log('Value:', this.selectedLunch.value);
+        console.log('Jméno uživatele:', this.selectedUser.username);
+        console.log('Value uživatele:', this.selectedUser.value);
         const selectedDateFormatted = new Date(this.selectedDate.value).toLocaleDateString('en-GB');
         console.log('selected date', selectedDateFormatted)
+
       } else {
         console.log('obed nenalezen');
       }
-
 
     }
   }
