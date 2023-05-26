79b0c8f4487e2f2dac3e3a8bf8d9cafbec021946
rehacsi <rehacsi@student.uzlabina.cz>
2023-05-26 12:04:26 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {TodolistComponent} from './todolist/todolist.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SelectComponent} from './select/select.component';
 import {ExamplesComponent} from './examples/examples.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -11,7 +11,11 @@ import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
-import { LunchComponent } from './lunch/lunch.component';
+import {LunchComponent} from './lunch/lunch.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -29,7 +33,12 @@ import { LunchComponent } from './lunch/lunch.component';
     MatSlideToggleModule,
     MatButtonToggleModule,
     MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/lunch/lunch.component.ts
+++ src/app/lunch/lunch.component.ts
@@ -1,26 +1,46 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LunchModel} from './lunch.model'
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-lunch',
   templateUrl: './lunch.component.html',
   styleUrls: ['./lunch.component.css']
 })
-export class LunchComponent {
+export class LunchComponent implements OnInit{
+  selectedDate = new FormControl
+  ngOnInit() {
+  }
 
-  selectedLunch: any;
-  lunchItems: any[] = [
+  today: number = Date.now();
+
+  showResult = false;
+  selectedLunchValue: number = -1;
+  selectedLunch: LunchModel | undefined = undefined;
+
+  lunchItems: LunchModel[] = [
     { name: 'Guláš', value: 1 },
     { name: 'Svíčková', value: 2 },
-    { name: 'Rajská', value: 3 }
+    { name: 'Rajská', value: 3 },
+    { name: "vegan", value: 4}
   ];
 
   submitForm() {
-    if (this.selectedLunch) {
-      const selected = this.selectedLunch - 1;
-      console.log('Oběd:', this.lunchItems[selected].name);
-      console.log('Value:', this.lunchItems[selected].value);
+    console.log('selected', this.selectedLunchValue)
+    console.log('lunch items', this.lunchItems)
+    if (this.selectedLunchValue !== null) {
+      this.selectedLunch = this.lunchItems.find((o) => o.value === +this.selectedLunchValue);
+
+      if (this.selectedLunch !== undefined) {
+        console.log('Oběd:', this.selectedLunch.name);
+        console.log('Value:', this.selectedLunch.value);
+        console.log('selected date', this.selectedDate.value)
+      } else {
+        console.log('obed nenalezen');
+      }
+
 
     }
   }
-}
 
+}
